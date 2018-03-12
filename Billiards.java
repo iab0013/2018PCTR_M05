84685428e83c245379e60d7a694b77105a5c9b85
jcr0069 <jcr0069@alu.ubu.es>
2018-03-12 19:21:26 +0100

    Creacion de variables
    Creamos las variables que nos serviran para gestionar todos los hilos y
    todas las bolas de nuestro programa

--- Billiards.java
+++ Billiards.java
@@ -19,8 +19,11 @@ public class Billiards extends JFrame {
 	private Board board;
 
 	// TODO update with number of group label. See practice statement.
-	private final int N_BALL = 2;
-	private Ball[] balls;
+	private final int N_BALL = 8;
+	private Ball[] balls = new Ball[N_BALL]; //bolas
+	private ThreadBall[] threadBalls = new ThreadBall[N_BALL];
+
+	private volatile boolean running = false;
 
 	public Billiards() {
 
